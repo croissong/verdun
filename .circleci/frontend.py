4e2b222d28ba23af18ac96bc3539e622b7000da8
Jan Moeller <jan.moeller0@gmail.com>
2019-05-26 02:45:43 +0200

    Mv frontend to root; Update circleci conf

--- .circleci/frontend.py
+++ .circleci/frontend.py
@@ -1,5 +1,4 @@
-CHANGE_CHECKER=./scripts/changed-since-last-commit.py
-SUB_DIR=./src/api
-if [[ `python3 ${CHANGE_CHECKER} ${SUB_DIR}` == "True" ]]; then
-    # run unit tests against ${SUB_DIR}
-fi
+def main():
+
+if __name__ == "__main__":
+    main()
