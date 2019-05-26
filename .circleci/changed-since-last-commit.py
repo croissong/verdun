ef43441b40c3c4fc68f707047581fc80e3d2361f
Jan Moeller <jan.moeller0@gmail.com>
2019-05-26 17:46:14 +0200

    (frontend) Update deps

--- .circleci/changed-since-last-commit.py
+++ .circleci/changed-since-last-commit.py
@@ -10,8 +10,9 @@ def main():
         sys.exit(1)
 
 def git_compare_url() -> str:
-    # something like: https://github.com/mkobit/gradle-test-kotlin-extensions/compare/211a8ef37eb6^...3c546b55628a
-    return os.getenv('CIRCLE_COMPARE_URL', '')
+    with open('workspace/CIRCLE_COMPARE_URL.txt', 'r') as file:
+        CIRCLE_COMPARE_URL = file.read()
+    return CIRCLE_COMPARE_URL
 
 
 # https://discuss.gradle.org/t/build-scan-plugin-1-10-3-issue-when-using-a-url-with-a-caret/24965
@@ -24,10 +25,12 @@ def get_current_run_commit() -> str:
 
 
 def changed_since_last_run_commit(dirs: list) -> bool:
-    if not git_compare_url():
+    compare_url = git_compare_url()
+    if not compare_url:
         print("CIRCLE_COMPARE_URL missing")
         return True
     else:
+        print(f'Compare url: {compare_url}')
         # We redirect stderr to stdout. If there is an error (e.g. in the first pipeline or some other edge cases), we regard everything as changed
         last_run_commit = get_last_run_commit()
         current_run_commit = get_current_run_commit()
