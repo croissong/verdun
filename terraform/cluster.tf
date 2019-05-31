0ef401c1d5b5486f0af21ca657e9a73ec1f152a8
Jan <jan.moeller0@gmail.com>
2019-06-01 01:24:33 +0200

    (traefik) Better tls ciphers; Extract auth-proxy deployment

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -2,11 +2,11 @@
 resource "digitalocean_kubernetes_cluster" "verdun" {
   name    = "verdun"
   region  = "fra1"
-  version = "1.14.1-do.2"
+  version = "1.14.2-do.0"
 
   node_pool {
     name       = "worker-pool"
-    size       = "s-1vcpu-2gb"
+    size       = "s-2vcpu-4gb"
     node_count = 1
     tags = ["verdun"]
   }
