5dba920b595ef4978736f28c7dea01bf5f401a14
Jan <jan.moeller0@gmail.com>
2019-12-22 00:41:05 +0100

    Mv traefik-middlewares into own chart

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -22,14 +22,6 @@ locals {
     "/.*worker-pool-([a-z0-9]+).*/",
     "worker-pool-$1",
   )
-  kube_config = jsonencode(
-    digitalocean_kubernetes_cluster.verdun.kube_config[0].raw_config,
-  )
-  cluster_context = replace(
-    local.kube_config,
-    "/.*current-context: ([a-z0-9-]+).*/",
-    "$1",
-  )
 }
 
 data "digitalocean_droplet" "verdun_node" {
@@ -79,12 +71,3 @@ resource "digitalocean_firewall" "verdun_infra" {
     source_addresses = ["0.0.0.0/0", "::/0"]
   }
 }
-
-output "cluster_context" {
-  value = local.cluster_context
-}
-
-output "cluster_id" {
-  value = local.cluster_id
-}
-
